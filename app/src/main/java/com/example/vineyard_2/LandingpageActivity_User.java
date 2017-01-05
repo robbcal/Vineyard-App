1d65d32dfa0ae436d3a9a5069a48961182313dd5
emilany <emilany.legaspi@gmail.com>
2017-01-05 09:32:38 +0800

    minor ui changes on login and signup

--- app/src/main/java/com/example/vineyard_2/LandingpageActivity_User.java
+++ app/src/main/java/com/example/vineyard_2/LandingpageActivity_User.java
@@ -11,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
