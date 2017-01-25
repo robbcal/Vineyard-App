1a00f41de0ee3d2db39ba3d121db4ffd5ca66299
Chiz <raychrisbelarmino@gmail.com>
2017-01-26 00:59:16 +0800

    fixed error sa guest na search page

--- app/src/main/java/com/example/vineyard_2/SearchFragment.java
+++ app/src/main/java/com/example/vineyard_2/SearchFragment.java
@@ -67,7 +67,7 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_search, container, false);
+        final View v = inflater.inflate(R.layout.fragment_search, container, false);
 
         mRecipeRef.keepSynced(true);
 
@@ -117,7 +117,7 @@ public class SearchFragment extends Fragment {
                     String ingredient = postSnapshot.child("ingredient").getValue(String.class);
                     ingredients.add(ingredient);
                 }
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), R.layout.auto_complete, ingredients);
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(v.getContext(), R.layout.auto_complete, ingredients);
                 searchField.setAdapter(adapter);
                 searchField.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
             }
